d4b263f73d64ce4cb7900323160872ce64334cc5
EvgenyKarikov <evgenij-karikov@mail.ru>
2021-04-15 01:17:22 +0300

    add background to mask labels

--- tryDeepLab3.m
+++ tryDeepLab3.m
@@ -7,51 +7,51 @@ gTruth=open('mask_gTruth.mat');
 
 imageSize = [416 416 3];
 
-numClasses = 2;
-% network = 'resnet18';
-% lgraph = deeplabv3plusLayers(imageSize,numClasses,network, ...
-%              'DownsamplingFactor',16);
+numClasses = 3;
+ network = 'resnet18';
+ lgraph = deeplabv3plusLayers(imageSize,numClasses,network, ...
+              'DownsamplingFactor',16);
 
-imgLayer = imageInputLayer(imageSize,"Name","input_1");
-filterSize = 3;
-numFilters = 32;
-conv = convolution2dLayer(filterSize,numFilters,'Padding',1);
-relu = reluLayer();
-poolSize = 2;
-maxPoolDownsample2x = maxPooling2dLayer(poolSize,'Stride',2);
-downsamplingLayers = [
-    conv
-    relu
-    maxPoolDownsample2x
-    conv
-    relu
-    maxPoolDownsample2x
-    ];
-
-filterSize = 4;
-transposedConvUpsample2x = transposedConv2dLayer(4,numFilters,'Stride',2,'Cropping',1);
-upsamplingLayers = [
-    transposedConvUpsample2x
-    relu
-    transposedConvUpsample2x
-    relu
-    ];
-
-conv1x1 = convolution2dLayer(1,numClasses);
-finalLayers = [
-    conv1x1
-    softmaxLayer()
-    pixelClassificationLayer()
-    ];
-
-
-
-lgraph = [
-    imgLayer    
-    downsamplingLayers
-    upsamplingLayers
-    finalLayers
-    ];
+% imgLayer = imageInputLayer(imageSize,"Name","input_1");
+% filterSize = 3;
+% numFilters = 32;
+% conv = convolution2dLayer(filterSize,numFilters,'Padding',1);
+% relu = reluLayer();
+% poolSize = 2;
+% maxPoolDownsample2x = maxPooling2dLayer(poolSize,'Stride',2);
+% downsamplingLayers = [
+%     conv
+%     relu
+%     maxPoolDownsample2x
+%     conv
+%     relu
+%     maxPoolDownsample2x
+%     ];
+% 
+% filterSize = 4;
+% transposedConvUpsample2x = transposedConv2dLayer(4,numFilters,'Stride',2,'Cropping',1);
+% upsamplingLayers = [
+%     transposedConvUpsample2x
+%     relu
+%     transposedConvUpsample2x
+%     relu
+%     ];
+% 
+% conv1x1 = convolution2dLayer(1,numClasses);
+% finalLayers = [
+%     conv1x1
+%     softmaxLayer()
+%     pixelClassificationLayer()
+%     ];
+% 
+% 
+% 
+% lgraph = [
+%     imgLayer    
+%     downsamplingLayers
+%     upsamplingLayers
+%     finalLayers
+%     ];
 
         
          
@@ -59,8 +59,10 @@ lgraph = [
 options = trainingOptions('adam', ...
        'InitialLearnRate', 0.001, ...
        'Verbose',true, ...
+       'validationData',[],...
+       'validationFrequency',15,...
        'MiniBatchSize',2, ...
-       'MaxEpochs',30, ...
+       'MaxEpochs',300, ...
        'Plots','training-progress', ...
        'Shuffle','every-epoch', ...
        'VerboseFrequency',10); 
